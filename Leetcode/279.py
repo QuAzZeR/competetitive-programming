7f6ea5c549a9bc4bd7c539d828efed779516df26
quazzer <natsamiss@gmail.com>
2024-02-08 21:36:26 +0700

    :sparkles: Finish Day8

---
+++ Leetcode/279.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            min_val = float("inf")
+            j = 1
+            while j * j <= i:
+                min_val = min(min_val, dp[i - j * j] + 1)
+                j += 1
+            dp[i] = min_val
+        return int(dp[n])
+
+
+print(Solution().numSquares(12))
+print(Solution().numSquares(13))
