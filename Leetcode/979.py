d1d1f6516ebb30036841c955d4b3804cc9457ad1
quazzer <natsamiss@gmail.com>
2024-05-18 12:53:45 +0700

    :sparkles: Finish Day18 Leetcode Challenge

---
+++ Leetcode/979.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, parent):
+            if node is None:
+                return 0
+            move = dfs(node.left, root) + dfs(node.right, root)
+            x = node.val - 1
+            if parent is not None:
+                parent.val += x
+            move += abs(x)
+            return move
+
+        return dfs(root, None)
